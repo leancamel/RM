caa9a4de4da622a5d0de751a04defc79eb0e180c
lin <1604478759@qq.com>
2023-03-01 22:19:20 +0800

    云台继电器完成

--- Hero/User/hardware/relays.c
+++ Hero/User/hardware/relays.c
@@ -3,25 +3,26 @@
 
 void Relays_On(void)
 {
-	GPIO_SetBits(GPIOB,GPIO_Pin_12);
+	GPIO_SetBits(GPIOC,GPIO_Pin_6);
 }
 
 void Relays_Off(void)
 {
-	GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+	GPIO_ResetBits(GPIOC,GPIO_Pin_6);
 }
 
 void Relays_Init(void)
 {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_High_Speed;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 	Relays_On();
-	return;
 }
