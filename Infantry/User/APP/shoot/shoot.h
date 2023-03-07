181142fe9c6170be02d45016a893e5eb5601da16
p-xiexin <1014575731@qq.com>
2023-03-07 21:52:38 +0800

    步兵发射机构参数调整

--- Infantry/User/APP/shoot/shoot.h
+++ Infantry/User/APP/shoot/shoot.h
@@ -53,8 +53,8 @@
 #define MOTOR_ECD_TO_ANGLE          0.000021305288720633905968306772076277f   // PI / (8192*180)
 #define FULL_COUNT                  18  //  36/2
 //拨弹速度
-#define TRIGGER_SPEED               10.0f
-#define CONTINUE_TRIGGER_SPEED      15.0f
+#define TRIGGER_SPEED               7.0f
+#define CONTINUE_TRIGGER_SPEED      12.0f
 #define READY_TRIGGER_SPEED         5.0f
 
 #define KEY_OFF_JUGUE_TIME          500
--- Infantry/User/hardware/trigger.h
+++ Infantry/User/hardware/trigger.h
@@ -2,8 +2,8 @@
 #define TRIGGER_H
 #include "main.h"
 
-#define FRIC_UP 1480
-#define FRIC_DOWN 1400
+#define FRIC_UP 1580
+#define FRIC_DOWN 1500
 #define FRIC_OFF 1000
 
 void trigger_PWM_Init(void);
