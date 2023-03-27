fb2ce9d64f6cd21ac09436fa913e9c35a06c498f
p-xiexin <1014575731@qq.com>
2023-03-27 23:55:14 +0800

    裁判系统DMA串口初始化

--- Infantry/User/main.h
+++ Infantry/User/main.h
@@ -22,9 +22,10 @@ typedef double fp64;
 
 #define RC_NVIC 4
 #define SPI1_RX_NVIC 5
-#define CAN1_NVIC 4
-#define CAN2_NVIC 4
-#define ROS_NVIC 1
+#define CAN1_NVIC 3
+#define CAN2_NVIC 3
+#define ROS_NVIC 4
+#define REFEREE_NVIC 4
 #define IST8310_NVIC 5
 #define BMI088_NVIC 5
 #define SoftWare_NVIC 5
